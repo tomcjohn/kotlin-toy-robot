688c4bbd1019c354aa1f3db4ae2c10f36b23082b
Tom John <tom.john@rea-group.com>
2021-01-18 14:59:51 +1100

    Add Gradle config

--- src/main/com/tomcjohn/robot/Main.kt
+++ src/main/com/tomcjohn/robot/Main.kt
@@ -4,10 +4,6 @@ import com.tomcjohn.robot.domain.Direction
 import com.tomcjohn.robot.domain.Position
 import com.tomcjohn.robot.domain.Robot
 
-class Main {
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            println(Robot(Position(1,1), Direction.NORTH))
-        }
-    }
+fun main(args: Array<String>) {
+    println(Robot(Position(1,1), Direction.NORTH))
 }
