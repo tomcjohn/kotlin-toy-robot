2316bea5d09067141b0f87fb99c9bc7aea44c122
Tom John <tom.john@rea-group.com>
2021-01-18 15:42:30 +1100

    Working on project structure

--- src/test/kotlin/com/tomcjohn/robot/domain/RobotTest.kt
+++ src/test/kotlin/com/tomcjohn/robot/domain/RobotTest.kt
@@ -1,10 +1,11 @@
 package com.tomcjohn.robot.domain
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class RobotTest () {
     @Test
     fun testMove() {
-
+        assertEquals(true, true)
     }
 }
